c3996d038e82cfdb0a1f79e7021b4fc5d433ee6c
AngieBrilliance <angie@codeforprogress.org>
2014-05-20 14:37:49 -0400

    Configured for Heroku Deployment

--- app/views.py
+++ app/views.py
@@ -3,5 +3,19 @@ from app import app
 from flask import render_template
 
 @app.route('/')
+@app.route('/index')
 def index():
 	return render_template('index.html')
+
+@app.route('/LandingPage')
+def newsfeed():
+	return render_template('LandingPage.html')
+
+@app.route('/LawyerLogin')
+def newsfeed():
+	return render_template('LawyerLogin.html')
+
+@app.route('/SignUp')
+def newsfeed():
+	return render_template('SignUp.html')
+	
